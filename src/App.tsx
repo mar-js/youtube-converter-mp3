1ebc0d19606f485c0581d2f96407c80073564aa0
Martín <maguera37@gmail.com>
2023-02-02 19:05:55 -0300

    feat: I added the main components in App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,22 @@
-export const App: React.FC = () => <div>aaa</div>
+import { Container } from '@chakra-ui/react'
+import { Form, Title } from 'components'
+
+export const App: React.FC = () => (
+  <Container
+    h="100vh"
+    maxW="100%"
+    bgGradient="linear(to-b, orange.100, purple.100, purple.200, purple.300, purple.400)"
+  >
+    <Container
+      h="100%"
+      maxW="container.sm"
+      py={ 5 }
+      display="flex"
+      justifyContent="center"
+      flexDirection="column"
+    >
+      <Title />
+      <Form />
+    </Container>
+  </Container>
+)
