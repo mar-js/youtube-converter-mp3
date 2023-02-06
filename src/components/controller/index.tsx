6a9db013651ab884400a069288235f4f62eeb161
Martín <maguera37@gmail.com>
2023-02-06 18:19:55 -0300

    fix: I center loader in controller

--- src/components/controller/index.tsx
+++ src/components/controller/index.tsx
@@ -1,5 +1,6 @@
 import { useData } from 'contexts'
 
+import { Box } from '@chakra-ui/react'
 import {
   Alert,
   Download,
@@ -10,12 +11,12 @@ export const Controller: React.FC = () => {
   const { data } = useData()
 
   return (
-    <>
+    <Box textAlign="center">
       { data?.status === 'processing' && <Loader /> }
 
       { data?.status === 'fail' && <Alert msg={ data?.msg as string } /> }
 
       { data?.status === 'ok' && <Download title={ data?.title as string } link={ data?.link as string } /> }
-    </>
+    </Box>
   )
 }
