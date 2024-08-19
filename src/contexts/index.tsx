96675b0ff451f7e42b855d00cd1505df1f51dfda
Martin <maguera37@gmail.com>
2024-08-19 11:34:12 -0300

    refactor contexts folder

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,1 +1,9 @@
-export { DataContext, useData } from './data'
+import { createContext, useContext } from "react";
+
+import type { IDataContextModel } from "interfaces";
+
+export const DataContext = createContext<IDataContextModel>(
+	{} as IDataContextModel,
+);
+
+export const useData = (): IDataContextModel => useContext(DataContext);
