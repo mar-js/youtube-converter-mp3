56958c802b74c47406513f3ebb430c99afbab520
Martin <maguera37@gmail.com>
2024-08-19 15:07:28 -0300

    correction path alias in vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,13 @@ export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
-			"@components/*": resolve(__dirname, "src/components/*"),
-			"@apis/*": resolve(__dirname, "src/apis/*"),
-			"@contexts": resolve(__dirname, "src/contexts/index.tsx"),
-			"@providers": resolve(__dirname, "src/providers/index.tsx"),
-			"@interfaces": resolve(__dirname, "src/interfaces/index.ts"),
-			"@helpers/*": resolve(__dirname, "src/helpers/*"),
-			"@utils/*": resolve(__dirname, "src/utils/*"),
+			"@components": resolve(__dirname, "./src/components"),
+			"@apis": resolve(__dirname, "./src/apis"),
+			"@contexts": resolve(__dirname, "./src/contexts/index.tsx"),
+			"@providers": resolve(__dirname, "./src/providers/index.tsx"),
+			"@interfaces": resolve(__dirname, "./src/interfaces/index.ts"),
+			"@helpers": resolve(__dirname, "./src/helpers"),
+			"@utils": resolve(__dirname, "./src/utils"),
 		},
 	},
 	server: {
