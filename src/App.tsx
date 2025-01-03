e1de1663990fb36d12bf82ff62688f88101f02a8
Martin <maguera37@gmail.com>
2025-01-03 18:19:35 -0300

    feat: change style to App component

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,24 @@ export const App: React.FC = () => {
 	const { data } = useData();
 
 	return (
-		<main className="relative h-screen flex flex-col justify-center items-center bg-slate-600 text-center px-5">
-			<Title />
-			{data ? <Controller /> : <Form />}
+		<main className="h-screen flex flex-col justify-center items-center bg-retro-brown-light-two text-center px-5">
+			<section className="bg-retro-brown-light-one p-5 rounded-lg outline outline-1 outline-retro-brown-dark-one mb-5">
+				<Title />
+				{data ? <Controller /> : <Form />}
+			</section>
 
-			<footer className="absolute bottom-5 flex justify-center items-center gap-1.5 text-white opacity-50">
-				@ Todos los derechos reservados |
+			<footer className="flex flex-col justify-center items-center text-retro-brown-dark-two">
+				<span>
+					© {new Date().getFullYear()} Retro Converter | Todos los derechos
+					reservados
+				</span>
 				<a
 					href="https://github.com/mar-js"
 					target="_blank"
 					rel="noopener noreferrer"
 					className="hover:underline"
 				>
-					mar-js
+					mar-js 🧑🏻‍💻
 				</a>
 			</footer>
 		</main>
