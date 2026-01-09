6a6c4e2e17e9328d99a7649b8d6ea26044a59721
Martin <maguera37@gmail.com>
2026-01-09 16:42:47 -0300

    feat: updated Title component with neon design styles and animations

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,5 +1,6 @@
 export const Title: React.FC = () => (
-	<h1 className="text-retro-brown-dark-one font-semibold text-5xl sm:text-6xl mb-10">
-		Youtube <br /> Converter MP3
+	<h1 className="text-neonPink font-futuristic font-bold text-5xl sm:text-6xl mb-10 drop-shadow-[0_0_16px_#ff00cc] animate-pulse">
+		Youtube <span className="text-neonBlue">Converter</span>{" "}
+		<span className="text-neonGreen">MP3</span>
 	</h1>
 );
